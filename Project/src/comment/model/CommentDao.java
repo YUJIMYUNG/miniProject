b77931db76b46482dae3e89cbc90c785dcbb179b
JIMYUNG_YU <jm_7516@naver.com>
2024-12-02 21:31:47 +0900

    test

--- Project/src/comment/model/CommentDao.java
+++ Project/src/comment/model/CommentDao.java
@@ -1,4 +1,7 @@
 package comment.model;
 
 public class CommentDao {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
--- Project/src/comment/model/CommentDto.java
+++ Project/src/comment/model/CommentDto.java
@@ -1,4 +1,6 @@
 package comment.model;
 
 public class CommentDto {
+    // 코드으ㅡ으니ㅏㅇㄹ니다류ㅜ니아류ㅟ나ㅠ우린
+
 }
