a9257515c2f1afcac6cfef8704a885cd4c4dffc3
HaJhin <apffksls333@naver.com>
2024-12-06 02:31:56 +0900

    VoteDao-JDBC 연동

--- Project/src/model/VoteDao.java
+++ Project/src/model/VoteDao.java
@@ -1,11 +1,31 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class VoteDao {
 
+    private Connection conn; // DB와 연동하는 객체
+
     private static VoteDao voteDao = new VoteDao();
-    private VoteDao() {}
+    private VoteDao() {
+        try {
+            // "teamDB" 데이터베이스와 연결
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/teamDB", "team1", "1234");
+            System.out.println("[ BoardDB Connection OK ]");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("[ BoardDB Connection fail ]");
+        }
+    } // 생성자 ed
     public static VoteDao getInstance() {
         return voteDao;
     } // 싱글턴 ed
+
+    // 1.투표 작성 함수
+    public void VoteFunction() {}
 
 } // VoteDao ed
--- Project/src/view/VoteView.java
+++ Project/src/view/VoteView.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.Scanner;
+
 public class VoteView {
 
     private static VoteView voteView = new VoteView();
@@ -7,5 +9,16 @@ public class VoteView {
     public static VoteView getInstance() {
         return voteView;
     } // 싱글턴 ed
+
+    Scanner scanner = new Scanner(System.in); // 스캐너 입력 객체
+
+    // 투표
+
+    // 1. 투표 작성 함수
+    public void VoteFunction() {
+        // BoardView.getInstance().boardPrint();
+        System.out.println("투표할 게시물 선택 : ");
+        int choose = scanner.nextInt();
+    }   /// 현재 VoteView 클래스 작업 중...
 
 } // VoteView ed
