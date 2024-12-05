50f538933a5f7a08ec8070f3e8a7ff30644f6f6f
HaJhin <apffksls333@naver.com>
2024-12-05 21:51:14 +0900

    VoteView 클래스 작업 중

--- Project/src/view/VoteView.java
+++ Project/src/view/VoteView.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.Scanner;
+
 public class VoteView {
 
     private static VoteView voteView = new VoteView();
@@ -8,4 +10,16 @@ public class VoteView {
         return voteView;
     } // 싱글턴 ed
 
+    Scanner scanner = new Scanner(System.in); // 스캐너 입력 객체
+
+    // 투표
+
+    // 1. 투표 작성 함수
+    public void VoteFunction() {
+     // BoardView.getInstance().boardPrint();
+        System.out.println("투표할 게시물 선택 : ");
+        int choose = scanner.nextInt();
+    }   /// 현재 VoteView 클래스 작업 중...
+
+    //
 } // VoteView ed
