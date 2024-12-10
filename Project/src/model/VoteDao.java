90d12af082b6a957d41991e9d745e34fe29aa95b
HaJhin <apffksls333@naver.com>
2024-12-10 13:48:04 +0900

    12/10.

--- Project/src/model/VoteDao.java
+++ Project/src/model/VoteDao.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class VoteDao extends Dao{
 
     private static VoteDao voteDao = new VoteDao();
@@ -8,13 +11,20 @@ public class VoteDao extends Dao{
         return voteDao;
     } // 싱글턴 ed
 
-    // 1.투표 작성 함수
+    /// 1.투표 작성 함수
     public boolean VoteWrite(VoteDto voteDto) {
         try {
-            String sql = "insert into vote(vote_content"
+            String sql = "insert into vote(vote_content,vote_deadline) values(?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1,voteDto.getVote_content());
+
+        } catch (SQLException e) {
+            e.getMessage();
         }
         boolean result = true;
         return result;
     }
+
+    /// 2.투표 조회 및 투표 함수
 
 } // VoteDao ed
--- Project/src/view/VoteView.java
+++ Project/src/view/VoteView.java
@@ -5,6 +5,8 @@ import model.BoardDto;
 import model.MemberDto;
 import model.VoteDto;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,29 +20,41 @@ public class VoteView {
 
     Scanner scanner = new Scanner(System.in); // 스캐너 입력 객체
 
-    // 1. 투표 생성 함수
+    /// 1. 투표 생성 함수
     public void VoteWrite() {
-        System.out.println("투표 내용 작성 : "); // 투표 내용 입력
+
+        // 투표 내용 입력
+        System.out.print("투표 내용 입력 : ");
         String vote_content = scanner.nextLine();
         scanner.nextLine();
 
-        ArrayList<String> votedtos = new ArrayList<>(); // 선택지 저장 객체 생성
+        // 투표 마감 날짜 입력
+        System.out.print("투표 마감 날짜 입력(YYYY-MM-DD HH:mm) : ");
+        String input = scanner.nextLine();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime deadLine = LocalDateTime.parse(input, formatter);
+
+        // 투표 선택지 입력
+        ArrayList<String> votedtos_choices = new ArrayList<>(); // 선택지 저장 객체 생성
         while (true) {
             System.out.print("1.선택지 입력 2.선택지 입력 종료 : "); // 선택지 입력을 받는 함수
             int choose = scanner.nextInt();
             if (choose == 1) {
                 System.out.print("선택지 내용 입력 : ");
                 String content = scanner.next();
-                votedtos.add(content);
+                votedtos_choices.add(content);
+                // 선택지를 받아 리스트에 저장
                 System.out.println("선택지 입력 완료");
             } else if (choose == 2) {
+                System.out.println("선택지 입력 종료");
                 break;
             } else {
                 System.out.println("유효하지 않은 입력입니다.");
             }
         } // while ed
 
-        boolean result = VoteController.getInstance().VoteWrite(vote_content,votedtos);
+        // 투표 내용과 선택지를 저장한 객체를 컨트롤러로 보내고 결과 수신
+        boolean result = VoteController.getInstance().VoteWrite(vote_content,deadLine,votedtos_choices);
         if (result) {
             System.out.println("투표 작성 성공");
         } else {
